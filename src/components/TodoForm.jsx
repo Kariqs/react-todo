97151c2822a241a4829a35cd34b8ce2e0ee48b57
unknown <kariukibenard189@gmail.com>
2024-04-12 11:38:58 +0300

    Made some changes

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,15 +2,37 @@ import styles from "./TodoForm.module.css";
 import React from "react";
 
 const TodoForm = () => {
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+  };
+
+  const inputChangeHandler = (input, value) => {
+    console.log([input] + ":" + value);
+  };
   return (
     <React.Fragment>
       <div className={styles.formdiv}>
-        <form action="">
+        <form onSubmit={submitFormHandler} action="">
           <h3>Create a Todo</h3>
           <label htmlFor="todo">Enter todo title</label>
-          <input type="text" id="todo" name="todo" />
+          <input
+            onChange={(event) => {
+              inputChangeHandler("todo", event.target.value);
+            }}
+            type="text"
+            id="todo"
+            name="todo"
+          />
           <label htmlFor="desc">Describe the todo</label>
-          <textarea name="" id="desc" cols="30" rows="10"></textarea>
+          <textarea
+            onChange={(event) => {
+              inputChangeHandler("desc", event.target.value);
+            }}
+            name=""
+            id="desc"
+            cols="30"
+            rows="10"
+          ></textarea>
           <div className={styles.buttondiv}>
             <button type="submit">Save</button>
           </div>
